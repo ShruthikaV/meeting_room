ea0b437ffee64a0a2348666e8bc5627e52d7f3b2
ShruthikaV <Shruthika789@gmail.com>
2023-12-05 09:08:57 +0400

    login_page

--- myblog/src/pages/Login.js
+++ myblog/src/pages/Login.js
@@ -18,58 +18,35 @@ const login = () => {
     console.log("Name:", name);
     console.log("Email:", email);
 
-    if (!name || !email) {
-      console.error("Name and email are required.");
-      return;
-    }
-
-    try {
-      const response = await fetch("https://sample-api.com/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, email }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      console.log("Authentication successful!");
-    } catch (error) {
-      console.error("Error during authentication:", error);
-    }
+    return (
+      <Box maxW="md" mx="auto" mt="20">
+        <Heading mb="4">Login</Heading>
+        <form onSubmit={handleLogin}>
+          <FormControl id="name" isRequired>
+            <FormLabel>Full Name</FormLabel>
+            <Input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter your Full Name"
+            />
+          </FormControl>
+          <FormControl id="email" mt="4" isRequired>
+            <FormLabel>Email address</FormLabel>
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+            />
+          </FormControl>
+          <Button mt="4" colorScheme="blue" type="submit">
+            Login
+          </Button>
+        </form>
+      </Box>
+    );
   };
-
-  return (
-    <Box maxW="md" mx="auto" mt="20">
-      <Heading mb="4">Login</Heading>
-      <form onSubmit={handleLogin}>
-        <FormControl id="name" isRequired>
-          <FormLabel>Full Name</FormLabel>
-          <Input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Enter your Full Name"
-          />
-        </FormControl>
-        <FormControl id="email" mt="4" isRequired>
-          <FormLabel>Email address</FormLabel>
-          <Input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter your email"
-          />
-        </FormControl>
-        <Button mt="4" colorScheme="blue" type="submit">
-          Login
-        </Button>
-      </form>
-    </Box>
-  );
 };
 
 export default login;
